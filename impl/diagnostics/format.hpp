c52eb1c91935bc9dff7da3eb885dbbddcb62293a
diogo <diogoalexandredecodasilvapt@gmail.com>
2026-05-19 09:48:14 +0100

    format small error

--- impl/diagnostics/format.hpp
+++ impl/diagnostics/format.hpp
@@ -7,7 +7,7 @@ namespace zith::diag {
 
 template <typename... Args>
 [[nodiscard]] inline std::string diag_format(std::string_view fmt, Args&&... args) {
-    return std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+    return std::vformat(fmt, std::make_format_args(args...));
 }
 
 } // namespace zith::diag
