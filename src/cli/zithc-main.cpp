008fd7127a9dff8e30a2cee45d996f2de5eba205
al24254 <24254@aeparede.edu.pt>
2026-06-11 11:21:22 +0100

    Pluh, bro forget to put stuff on place

--- src/cli/zithc-main.cpp
+++ src/cli/zithc-main.cpp
@@ -1,14 +1,7 @@
 #include "cli/compiler-driver.hpp"
 #include "cli/options.hpp"
-#include <cstdio>
-#include <cstdlib>
 
-void foo(){
-	for(int i = 32; i <= 126; i++) printf("%c\n", i);
-}
-
-int main(int argc, char **argv) {
-    /*auto opts = zith::cli::parseArgs(argc, argv);
-    return zith::cli::CompilerDriver::run(opts);*/
-	foo();
+int main(const int argc, char **argv) {
+    const auto opts = zith::cli::parseArgs(argc, argv);
+    return zith::cli::CompilerDriver::run(opts);
 }
