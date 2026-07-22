170a16548079150cbe771cb683f2cd775976ade0
diogo <diogoalexandredecodasilvapt@gmail.com>
2026-07-22 13:20:53 +0100

    fah

--- build/_deps/frozen-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diogo/Zith/build/_deps/frozen-src
-# Build directory: /home/diogo/Zith/build/_deps/frozen-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include/frozen")
--- build/_deps/frozen-build/include/frozen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diogo/Zith/build/_deps/frozen-src/include/frozen
-# Build directory: /home/diogo/Zith/build/_deps/frozen-build/include/frozen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
